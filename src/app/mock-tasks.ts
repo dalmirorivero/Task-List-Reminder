bdc2d06cdc31a184f88568b2f32af5c8cd93dc4c
Dalmiro Rivero <dalmiro.rivero1@gmail.com>
2022-10-30 18:56:49 -0300

    Initial commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from "./Task"
+// Fake DB with Tasks
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Terminar primer módulo de Angular',
+        day: 'Agosto 5 a las 12:00',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Hacer Compras',
+        day: 'Agosto 6 a las 18:00',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Investigar Bootstrap',
+        day: 'Agosto 7 a las 16:00',
+        reminder: true
+    },
+    {
+        id: 4,
+        text: 'Migrar Portfolio',
+        day: 'Agosto 8 a las 13:00',
+        reminder: true
+    }
+]
